8daee0cab191fb53ef95cd200be5f3f5b059b602
snowalw5 <122179565+snowalw5@users.noreply.github.com>
2023-01-08 15:15:14 -0500

    Update streamlit_app.py
    created variable called fruits_selected that has the list of selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,13 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # This line filters the table data and prepopulates the list for the customer
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
+# put the list of selected fruits into a variable called fruits_selected
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#Then, we'll ask our app to use the fruits in our fruits_selected list to pull rows from the full data set 
+#(and assign that data to a variable called fruits_to_show). 
+#Finally, we'll ask the app to use the data in fruits_to_show in the dataframe it displays on the page.  
+
 
 
 
