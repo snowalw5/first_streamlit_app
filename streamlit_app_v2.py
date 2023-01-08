ba82aeb60abdf2c679cbc10949f729e2fa7a9fa4
snowalw5 <122179565+snowalw5@users.noreply.github.com>
2023-01-08 14:37:34 -0500

    Create streamlit_app_v2.py
    Trying to get the import file to work

---
+++ streamlit_app_v2.py
@@ -0,0 +1,13 @@
+import streamlit
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🧃 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍓🍌 Build Your Own Fruit Smoothie 🍉🍊')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
