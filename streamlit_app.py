69ecaecb24bd64e917a1c74e1e9372da561b744d
snowalw5 <122179565+snowalw5@users.noreply.github.com>
2023-01-08 14:29:19 -0500

    Update streamlit_app.py
    Import pandas and pull csv file from s3 bkt to pandas df called my_fruit_list, and display on the streamlit page

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍓🍌 Build Your Own Fruit Smoothie 🍉🍊')
 
-
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
